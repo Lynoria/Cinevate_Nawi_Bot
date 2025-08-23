9bf71672cd245ea4c6716e396f5774335d6125d5
Anastasia <nn220308@gmail.com>
2025-08-23 20:22:59 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nawi-bot",
+    version="1.0",
+    install_requires=[
+        "pyTelegramBotAPI==4.14.0",
+        "requests==2.31.0", 
+        "python-dotenv==1.0.0"
+    ]
+)
